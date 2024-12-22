a014aee39841a3ca1bb1adfd6cf01d0b670eb400
sameh402 <66880178+sameh402@users.noreply.github.com>
2024-12-22 06:12:19 -0800

    Add files via upload

---
+++ Calculator/Operation.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Operation
+    {
+        public string addition(int num1, int num2)
+        {
+            return $"{num1} + {num2} = {num1 + num2}";
+        }
+        public string subtraction(int num1, int num2)
+        {
+            return $"{num1} - {num2} = {num1 - num2}";
+        }
+        public string multiply(int num1, int num2)
+        {
+            return $"{num1} x {num2} = {num1 * num2}";
+        }
+        public string division(int num1, int num2)
+        {
+            return $"{num1} / {num2} = {num1 / num2}";
+        }
+        public string even_odd(int num)
+        {
+            if (num % 2 == 0)
+            {
+                return $"{num} is even";
+            }
+            else
+            {
+                return $"{num} is even";
+
+            }
+        }
+        public int factorial(int num)
+        {
+            if (num == 0 || num == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return num * factorial(num - 1);
+            }
+        }
+        public int fibonacci(int num)
+        {
+            if (num == 0 || num == 1)
+            {
+                return num;
+            }
+            else
+            {
+                return fibonacci(num - 1) + fibonacci(num - 2);
+            }
+        }
+    }
+}
---
+++ Calculator/UserInterface.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class UserInterface
+    {
+        public void GreedMessage()
+        {
+            Console.WriteLine("Welcome To Takamul Calculator");
+        }
+        public void Operations()
+        {
+            Console.WriteLine("Please Enter number of your chocie: ");
+            Console.WriteLine("1. addition");
+            Console.WriteLine("2. subtraction");
+            Console.WriteLine("3. multiplication");
+            Console.WriteLine("4. division");
+            Console.WriteLine("5. even_odd");
+            Console.WriteLine("6. factorial");
+            Console.WriteLine("7. fibonacci");
+            Console.WriteLine("8. exit");
+        }
+    }
+}
